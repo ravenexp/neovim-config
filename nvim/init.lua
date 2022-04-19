ab4219f6c89c703edba41d203f382557b51835cf
Sergey Kvachonok <ravenexp@gmail.com>
2022-04-20 00:39:59 +0300

    Initial commit: Add `init.lua` config file

---
+++ nvim/init.lua
@@ -0,0 +1,254 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+require('packer').startup(function()
+  local use = require('packer').use
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'terrortylor/nvim-comment' -- "gc" to comment visual regions/lines
+  use 'godlygeek/tabular' -- Markdown table formattring
+  -- UI to select things (files, grep results, open buffers...)
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'Shatur/neovim-ayu' -- Ayu theme
+  -- Lua statusline
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  -- Add git related info in the signs columns and popups
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'simrat39/rust-tools.nvim'
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'saadparwaiz1/cmp_luasnip'
+end)
+
+-- Enable line numbers
+vim.o.number = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Ignore when running ctags
+vim.o.wildignore = '.mypy_cache,.tox,*_venv,target'
+
+require('nvim_comment').setup()
+require('ayu').colorscheme()
+require('lualine').setup {
+  options = {
+    theme = 'ayu_dark',
+  }
+}
+
+-- Cyrillic
+vim.keymap.set('n', 'ш', 'i')
+
+-- Gitsigns
+require('gitsigns').setup()
+
+-- Telescope
+require('telescope').setup()
+
+--Add leader shortcuts
+do
+  local cmd = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>sb', cmd.buffers)
+  vim.keymap.set('n', '<leader>sf', cmd.find_files)
+  vim.keymap.set('n', '<leader>sh', cmd.help_tags)
+  vim.keymap.set('n', '<leader>sg', cmd.live_grep)
+end
+
+-- Treesitter configuration
+-- Parsers must be installed manually via :TSInstall
+require('nvim-treesitter.configs').setup {
+  ensure_installed = 'all',
+  highlight = {
+    enable = true, -- false will disable the whole extension
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+  },
+}
+
+-- LSP settings
+do
+  local cmd = vim.diagnostic
+  vim.keymap.set('n', '<leader>e', cmd.open_float)
+  vim.keymap.set('n', '<leader>q', cmd.setloclist)
+  vim.keymap.set('n', '[d', cmd.goto_prev)
+  vim.keymap.set('n', ']d', cmd.goto_next)
+end
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(_, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local cmd = vim.lsp.buf
+  local opts = { buffer = bufnr }
+  vim.keymap.set('n', 'gd', cmd.definition, opts)
+  vim.keymap.set('n', 'gD', cmd.declaration, opts)
+  vim.keymap.set('n', '<leader>D', cmd.type_definition, opts)
+  vim.keymap.set('n', 'gi', cmd.implementation, opts)
+  vim.keymap.set('n', 'gr', cmd.references, opts)
+  vim.keymap.set('n', 'K', cmd.hover, opts)
+  vim.keymap.set('n', '<C-k>', cmd.signature_help, opts)
+  vim.keymap.set('n', '<leader>rn', cmd.rename, opts)
+  vim.keymap.set('n', '<leader>ca', cmd.code_action, opts)
+  vim.keymap.set('n', '<leader>f', cmd.formatting, opts)
+end
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'clangd', 'pylsp' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+require('rust-tools').setup {
+  server = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+}
+
+-- Sumneko config
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require 'lspconfig'.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
