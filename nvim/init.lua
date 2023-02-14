29b6e888b522d548728a7432eff98497a989530c
Sergey Kvachonok <ravenexp@gmail.com>
2023-02-14 09:17:57 +0300

    fix: Replace deprecated 'sumneko_lua' with 'lua_ls'

--- nvim/init.lua
+++ nvim/init.lua
@@ -191,12 +191,12 @@ require('rust-tools').setup {
   }
 }
 
--- Sumneko config
+-- LUA LS config
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-lsp_config.sumneko_lua.setup {
+lsp_config.lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
